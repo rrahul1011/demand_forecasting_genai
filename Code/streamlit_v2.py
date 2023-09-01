7e433a3410449e8a75242c80fc639d211cde157c
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-09-01 19:04:34 +0530

    Update streamlit_v2.py

--- Code/streamlit_v2.py
+++ Code/streamlit_v2.py
@@ -13,7 +13,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 openai.api_key = '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'
 st.set_page_config(
             page_title="Sigmoid GenAI",
-            page_icon="Data/cropped-Sigmoid_logo_3x.pn."  
+            page_icon="/Data/cropped-Sigmoid_logo_3x.png."  
         )
 
 def select_country(d):
@@ -46,7 +46,7 @@ def select_level(d):
     return selected_levels, selected_channel, selected_brand, selected_SKU
 
 ##Reading the data
-df = pd.read_csv("Data/Retail_Data.csv")
+df = pd.read_csv("/Data/Retail_Data.csv")
 tab1, tab2 = st.tabs(["About the App", "App"])
 with tab2:
     st.header("The APP")
@@ -110,7 +110,7 @@ with tab2:
 
 
     ### Chat boat
-        df_user = pd.read_csv("/Users/rahulkushwaha/Desktop/GenAI_version_3/genAI_demand_forecasting/Data/Retail_Data.csv")
+        df_user = pd.read_csv("/Data/Retail_Data.csv")
         st.markdown("---")
         st.subheader("Have more Questions? \U0001F4AC")
         st.write("Head of the dataset")
